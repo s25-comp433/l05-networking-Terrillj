3185e27b2081c90c8ab8deb8290477b6561773b2
Jake Terrill <terrillj@unc.edu>
2025-03-02 13:56:12 -0500

    Complete requirements to display content about basketball games from sams api

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
+struct Game: Codable {
+    var id: Int
+    var team: String
+    var score: Score
+    var date: String
+    var opponent: String
+    var isHomeGame: Bool
+}
+
+
 struct ContentView: View {
+    @State private var results = [Game]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.id) { item in
+            HStack{
+                VStack(alignment: .leading){
+                    Text("\(item.team) vs. \(item.opponent)")
+                        .font(.headline)
+                    Text(item.date)
+                        .foregroundStyle(.secondary)
+                        .font(.caption)
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text("\(item.score.unc) - \(item.score.opponent)")
+                    Text(item.isHomeGame ? "Home" : "Away")
+                        .foregroundStyle(.secondary)
+                        .font(.caption)
+                }
+            }
+            
+        }.task{
+            await loadData()
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("invalid url")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Game].self, from: data) {
+                results = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
+        
     }
 }
 
